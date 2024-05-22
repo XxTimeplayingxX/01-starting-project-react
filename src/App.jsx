cc40254548df592245fc41c7a4a68c0e105e307e
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-05-21 21:22:21 -0500

    Cambios Ejercicio en clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,58 @@
-import reactImg from './assets/react-core-concepts.png';
-import compImg from './assets/components.png';
+const reactDescriptions = [
+  "Fundamental",
+  "Crucial",
+  "Core",
+  "Fundamentos",
+  "Messi",
+  "Cristiano",
+  "Dimitrakis",
+  "Sánchez",
+];
 
-import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const array = ["David", "Sánchez", 19, "ISTLC"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
+function HelloWorld() {
+  return <span>Hola Mundo</span>;
+}
+
+function HeaderComponent() {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
   return (
     <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>¿Qué miras Bobo?</h1>
       <p>
-        {description} React concepts you will need for almost any app you are going to build!
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
   );
 }
+function ArrayComponent() {
+  const david = array;
+  return(
 
-function CoreConcept(props){
-  return (
-    <li>
-      <img src={props.image} alt={props.title}/>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>      
-    </li>
+    <ul>
+    <li>Nombre: {david[0]}</li>
+    <li>Apellido: {david[1]}</li>
+    <li>Edad: {david[2]}</li>
+    <li>Lugar de Estudio: {david[3]}</li>
+  </ul>
   );
 }
 
 function App() {
   return (
     <div>
-      <Header />
+      <HeaderComponent />
       <main>
+        <HelloWorld />
         <h2>Time to get started!</h2>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept 
-            title="Components"
-            description="The code UI building block"
-            image={reactImg}
-            />
-            <CoreConcept 
-            title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-            image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept 
-            title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-            image={CORE_CONCEPTS[2].image}
-            />
-          </ul>
-        </section>
+        <ArrayComponent/>
       </main>
     </div>
   );
